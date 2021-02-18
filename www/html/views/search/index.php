c8f4bc697dc6229b7442e798e92d7da71a50ae1f
jad_109 <horiyuki9811@gmail.com>
2021-02-18 17:53:03 +0900

    changing top.php

--- www/html/views/search/index.php
+++ www/html/views/search/index.php
@@ -2,4 +2,37 @@
     //topページ
 ?>
 
-<p>This is search/index.php</p>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>支援総合検索サイト</title>
+    <link rel="stylesheet" href="stylesheet.css">
+
+    </head>
+    <body>
+        <header>
+            <div class="header">
+                <div class="header-logo">支援総合検索サイト
+                </div>
+                <div class="header-list">
+                    <ul>
+                        <li>主催者</li>
+                        <li>なぜこのサイトを始めたか</li>
+                        <li>お問い合わせ</li>
+                    </ul>    
+                </div>
+
+            </div>
+        </header>
+        <main>
+    
+
+        </main>
+        <footer>
+
+        </footer>
+
+
+    </body>
+</html>
