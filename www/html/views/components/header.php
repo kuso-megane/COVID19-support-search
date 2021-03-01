c6586b9212118e28e7b6623971f88513411c2ff0
jad_109 <horiyuki9811@gmail.com>
2021-03-01 20:24:19 +0900

    headerlist変更

--- www/html/views/components/header.php
+++ www/html/views/components/header.php
@@ -4,8 +4,7 @@
         
         <div class="header-list">
             <ul>
-                <li>主催者</li>
-                <li>なぜこのサイトを始めたか</li>
+                <li>サイト運営者</li>
                 <li>支援・制度利用時のお役立ち情報</li>
             </ul>    
         </div>
