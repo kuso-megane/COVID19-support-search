479202cc5244f005c4acaa90c40df1619312a290
kuso-megane <orikazu0824@oritawakeinoMacBook-puro.local>
2021-03-03 22:13:16 +0900

    views/components/footer.php をrequire

--- www/html/views/search/result.php
+++ www/html/views/search/result.php
@@ -30,16 +30,8 @@
                     <?php endfor; ?>
                 </div>
         </main>
-        <footer>
-   
-            <div class="footer-list">
-                <ul>
-                    <li>サイト運営者</li>
-                    <li>支援・制度利用時のお役立ち情報</li>
-                </ul>    
-            </div>    
-   
-        </footer>
+       
+        <?php require $componentsPath. 'footer.php'; ?>
 
 
     </body>
