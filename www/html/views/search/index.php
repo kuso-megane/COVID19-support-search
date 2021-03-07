133259b36ed2f6d2bac83b1dba0267e11ad9abdd
jad_109 <horiyuki9811@gmail.com>
2021-03-07 14:51:50 +0900

    メディアクエリ

--- www/html/views/search/index.php
+++ www/html/views/search/index.php
@@ -8,7 +8,7 @@
         <meta charset="utf-8">
         <title>支援総合検索サイト</title>
 
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="../../asset/stylesheet/index.css">
 
     </head>
