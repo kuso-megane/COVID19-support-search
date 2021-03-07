07219c118704f2aa32b08a837eaef6ff2be23f60
jad_109 <horiyuki9811@gmail.com>
2021-03-07 20:17:50 +0900

    検索ボックスを作るファイルをとりあえず作った

---
+++ www/html/views/components/searchbox.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>支援総合検索サイト</title>
+
+    <link rel="stylesheet" type="text/css" href="">
+
+    </head>
+    <body>
+
+        <main>
+
+
+            <div>
+                
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            </div>
+
+
+
+          
+
+        </main>
+
+   
+    </body>
+</html>
