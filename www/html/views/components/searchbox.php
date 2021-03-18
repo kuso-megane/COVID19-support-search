bc4d63cae17a5062e1af86d74d83a3e208c1b952
kuso-megane <orikazu0824@oritawakeinoMacBook-puro.local>
2021-03-18 16:06:46 +0900

    views search-box基礎設計

--- www/html/views/components/searchbox.php
+++ www/html/views/components/searchbox.php
@@ -1,38 +1,36 @@
-<!DOCTYPE html>
-<form action="" method="">
-    <div class="searching">
-        <div class="support box">
-            <p>困っていること</p>
-            <select name="support1" id="support1">
-                <option value="consulting">生活一般について相談</option>
-                <option value="house">住居入居支援</option>
-                <option value="foodbank">食料の無料提供</option>
-                <option value="eating">炊き出し</option>
-            </select>
-        </div>
-     
-        <div class="area box">
-            <p>地域</p>            
-            <select name="area">
-                <option value="">全国</option>
-                <option value="">東京</option>
-                <option value="">大阪</option>
-                <option value="">名古屋</option>
-            </select>
-        
-        </div>
 
-        <div class="condition box">
-            <p>気になる条件</p>
-            <input id="foreign" type="checkbox" name="condition" value="foreign"><label for="foreign">国籍不問</label>
-            <input id="public" type="checkbox" name="condition" value="public"><label for="public">公的支援のみ</label>
-            <input id="private" type="checkbox" name="condition" value="private"><label for="private">民間支援のみ</label>
-            
-        </div>
-        
-    </div> 
-    <div class="search box">   
-            <input type="submit" value="検索する🔍">
+<form method="post" action="">
+    <div id="search-box-container">
+        <div id="search-box">
+            <p class="block-start0">&lt;検索はこちら&gt;</p>
+            <div class="search-items">
+                <p><span class="bold">困っていること:</span></p>
+                <select name="support1" id="support1">
+                    <option value="consulting">生活一般について相談</option>
+                    <option value="house">住居入居支援</option>
+                    <option value="foodbank">食料の無料提供</option>
+                    <option value="eating">炊き出し</option>
+                </select>
+            </div>
+            <div class="search-items">
+                <p><span class="bold">地域:</span></p>        
+                <select name="area">
+                    <option value="">全国</option>
+                    <option value="">東京</option>
+                    <option value="">大阪</option>
+                    <option value="">名古屋</option>
+                </select>
+            </div>
+            <p class="search-items">
+                <span class="bold">
+                    <input id="is_foreign_ok" type="checkbox" name="is_foreign_ok"><label for="foreign">国籍不問</label>
+                </span>  
+            </p>
+            <p>
+                <input id="submit" type="submit" value="検索する🔍">
+            </p>  
+        </div> 
     </div>
-        
+    
+           
 </form>
--- www/html/views/search/index.php
+++ www/html/views/search/index.php
@@ -9,7 +9,7 @@
         <title>支援総合検索サイト</title>
 
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../../asset/stylesheet/index.css">
+        <link rel="stylesheet" type="text/css" href="../../asset/stylesheet/index.css">
 
     </head>
     <body>
@@ -20,7 +20,12 @@
         
             <div id="introduce">
                 <h2>困っていることにあった支援・相談先を探せる！検索システム</h2>
-                <p>「仕事を失った」「お金がない」、一人ではどうしようもない時<br>でも相談先がない。どんな支援があるか、受給資格があるかわからない<br><span>そんな時に、支援・相談先を簡単に探せるように、お手伝いさせていただきます！</span><br>★民間支援に<span>要件は基本的にありません</span>　諦める前に一度だけ検索してください</p>
+                <ul>
+                    <li>仕事を失った」「お金がない」、一人ではどうしようもない時でも相談先がない。</li>
+                    <li>どんな支援があるか、受給資格があるかわからない、、、、。</li>
+                </ul>
+                <p class="break-word"><span class="red">そんな時に、支援・相談先を簡単に探せるように、お手伝いさせていただきます！</span></p>
+                &#9733;民間支援に<span class="red">要件は基本的にありません</span>　諦める前に一度だけ検索してください</p>
             </div>
 
             <?php require $componentsPath. 'searchbox.php'; ?>
