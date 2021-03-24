4c7fc16dcd656f9e68ed8d8652495fdc2410ca25
kuso-megane <orikazu0824@oritawakeinoMacBook-puro.local>
2021-03-24 17:21:49 +0900

    SEO対策で検索結果表示ページのurlをsearchResultに変更

--- www/html/router/route.php
+++ www/html/router/route.php
@@ -12,7 +12,7 @@ $base = '/';
 $dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) use ($base) {
     $r->addRoute('GET', $base.'index[/{page:\d+}]', 'index'); //index
 
-    $r->addRoute('GET', '/search/{is_public_page:[0-1]}/{pub_p:\d+}/{pri_p:\d+}', 'searchResult');
+    $r->addRoute('GET', '/searchResult/{is_public_page:[0-1]}/{pub_p:\d+}/{pri_p:\d+}', 'searchResult');
     
 
     $r->addRoute('GET', '/article/list', 'articleList');
