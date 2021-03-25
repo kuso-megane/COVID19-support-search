1333aaf48e21f41d33cc7f1338a843a42afbe366
kuso-megane <orikazu0824@oritawakeinoMacBook-puro.local>
2021-03-25 19:30:52 +0900

    mysql/DB/init/init_app_db.sql#SupportOrgs ownerをnot uniqueに、access, support_content, appendixをもっと大きく

--- mysql/DB/init/init_app_db.sql
+++ mysql/DB/init/init_app_db.sql
@@ -11,7 +11,7 @@ create table AreaList(
 drop table if exists TroubleList; 
 create table TroubleList(
     id TINYINT UNSIGNED primary key auto_increment,
-    name varchar(30) NOT NULL unique,
+    name varchar(50) NOT NULL unique,
     meta_word varchar(50) NOT NULL
 );
 
@@ -19,13 +19,13 @@ drop table if exists SupportOrgs;
 create table SupportOrgs(
     id SMALLINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     area_id TINYINT UNSIGNED,
-    support_content varchar(200) NOT NULL,
-    owner varchar(100) NOT NULL unique,
-    access varchar(200) NOT NULL,
+    support_content varchar(400) NOT NULL,
+    owner varchar(100) NOT NULL,
+    access varchar(400) NOT NULL,
     is_foreign_ok BOOLEAN NOT NULL,
     is_public BOOLEAN NOT NULL,
     meta_words VARCHAR(100) NOT NULL,
-    appendix VARCHAR(50) default NULL,
+    appendix VARCHAR(200) default NULL,
 
     CONSTRAINT fk_area_id_on_SupportOrgs
         FOREIGN KEY (area_id)
