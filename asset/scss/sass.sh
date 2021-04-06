2e6cf103555d221d0ebc9eaf5da7c8a5a1b0c581
kuso-megane <orikazu0824@oritawakeinoMacBook-puro.local>
2021-04-07 00:26:42 +0900

    scss/search 整理

--- asset/scss/sass.sh
+++ asset/scss/sass.sh
@@ -2,5 +2,9 @@
 
 targetPath="../../www/html/asset/stylesheet/"
 
-sass search/search.scss ${targetPath}search/search.css
+sass search/index.scss ${targetPath}search/index.css
+sass search/result.scss ${targetPath}search/result.css
+
 sass article/articleList.scss ${targetPath}article/articleList.css
+
+sass subContents/guideline.scss ${targetPath}subContents/guideline.css
