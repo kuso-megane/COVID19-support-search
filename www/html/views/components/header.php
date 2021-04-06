e0e9096e474cb52a202d66fdf657c5f5ba1c63dd
kuso-megane <orikazu0824@oritawakeinoMacBook-puro.local>
2021-04-06 20:10:27 +0900

    title--right--menu--content 見た目だけ完成

--- www/html/views/components/header.php
+++ www/html/views/components/header.php
@@ -15,18 +15,36 @@
         
 
         <div id="title--right">
-            <p id="title--right--menu" class="block-start0 block-end0">
-                <i class="fas fa-caret-down"></i>
-                メニュー
-            </p>
-            
+            <div id="title--right--menu--container">
+                <p id="title--right--menu" class="block-start0 block-end0">
+                    <i class="fas fa-bars"></i> メニュー
+                </p>  
+            </div>
         </div>
-
     </div>
+
     <div id="link-to-top--container">
         <a id="link-to-top" href="/index">
             <i class="fas fa-home"></i>
             トップページへ
         </a>
+    </div>
+    <div id="title--right--menu--content" class="show">
+        <p id="close"><i class="far fa-times-circle fa-3x"></i></p>
+        <li>
+            <a href="/index#search-box">
+                <i class="fas fa-search"></i>
+                支援・相談先を検索
+            </a>
+        </li>
+        <li>
+            <a href="/article/list">
+                <i class="fas fa-book-open"></i>
+                お役立ち情報(コラム)
+            </a>
+        </li>
+        <li><a href="">運営方針</a></li>
+        <li><a href="">運営者紹介</a></li>
+        <li><a href="">お問い合わせ</a></li>
     </div>
 </header>
