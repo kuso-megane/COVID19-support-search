23f8f981f988f82b80f523e453d99db5b1b87871
kuso-megane <orikazu0824@oritawakeinoMacBook-puro.local>
2021-04-08 19:13:27 +0900

    views/components/confirmation.php 出現、消去のscript実装、buttonがformを送信してしまうのでdivに変更

--- www/html/views/components/confirmation.php
+++ www/html/views/components/confirmation.php
@@ -1,8 +1,32 @@
+<?php
+    /*
+     * class="confirmation--trigger"を押すと展開
+     * form要素専用
+     */
+?>
 
-<div id="confirmation">
+<div id="confirmation" class="hidden">
     <p>本当に実行しますか？</p>
     <div id="confirmation--button-container">
         <input id="confirmation--submit-button" class="confirmation--buttons" type="submit" value="確認">
-        <button id="confirmation--back-button" class="confirmation--buttons">戻る</button>
+        <div id="confirmation--back-button" class="confirmation--buttons">戻る</div>
     </div>
 </div>
+
+<script>
+    const confirmationTrigger = document.querySelector(".confirmation--trigger");
+    const backButton = document.getElementById("confirmation--back-button");
+
+    const confirmation = document.getElementById("confirmation");
+
+    const showConfirmation = () => {
+        confirmation.classList.remove("hidden");
+    }
+
+    const hiddenConfirmation = () => {
+        confirmation.classList.add("hidden");
+    }
+
+    confirmationTrigger.addEventListener("click", showConfirmation);
+    backButton.addEventListener("click", hiddenConfirmation);
+</script>
