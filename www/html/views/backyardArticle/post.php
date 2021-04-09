fc9414c7925c0afb7505960a72469f1d00bfb9db
kuso-megane <orikazu0824@oritawakeinoMacBook-puro.local>
2021-04-10 03:50:29 +0900

    BYArticle/post ページ作成

--- www/html/views/backyardArticle/post.php
+++ www/html/views/backyardArticle/post.php
@@ -1,5 +1,6 @@
 <?php
     use myapp\config\ViewsConfig;
+    use myapp\config\AppConfig;
 ?>
 
 <!DOCTYPE html>
@@ -9,6 +10,13 @@
         <?php require ViewsConfig::COMPONENTS_PATH. 'commonHead.php'; ?>
     </head>
     <body>
-        <?if  ?>
+        <?php if($vm === AppConfig::POST_SUCCESS): ?>
+            <p>コラムの追加・更新に成功しました。</p>
+        <?php else: ?>
+            <p>コラムの追加・更新に失敗しました。</p>
+        <?php endif; ?>
+
+        <p><a href="/backyard/article/index">コラム編集ページへ</a></p>
+        <p><a href="/backyard/index">管理画面トップページへ</a></p>
     </body>
 </html>
