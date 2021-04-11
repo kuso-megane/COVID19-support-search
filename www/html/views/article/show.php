cfb5a232006a7b29848483bde501013816d7429d
kuso-megane <orikazu0824@oritawakeinoMacBook-puro.local>
2021-04-11 11:56:01 +0900

    デザイン微調整

--- www/html/views/article/show.php
+++ www/html/views/article/show.php
@@ -24,7 +24,7 @@ use myapp\config\ViewsConfig;
                         <p><a href="/article/list"><i class="fas fa-book-open"></i>コラム一覧へ戻る</a></p>
                     </div>
                     <div id="article--title">
-                        <h3 class="block-end0 block-start0 center"><?php echo $articleContent['title']; ?></h3>
+                        <h2 class="block-end0 block-start0 center"><?php echo $articleContent['title']; ?></h3>
                     </div>
                     <div id="article--content"></div>
                     <div class="link-to-articleList">
