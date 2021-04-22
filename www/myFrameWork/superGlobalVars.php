2814d2cafe498e96dfe638ab801838bf52232706
kuso-megane <orikazu0824@oritawakeinoMacBook-puro.local>
2021-04-22 12:15:36 +0900

    myFW/SuperGrovalVars session関連実装

--- www/myFrameWork/superGlobalVars.php
+++ www/myFrameWork/superGlobalVars.php
@@ -34,4 +34,23 @@ class SuperGlobalVars
     {
         return $_FILES;
     }
+
+
+    public function getSession():array
+    {
+        return $_SESSION;
+    }
+
+    public function setSession(array $vars)
+    {
+        foreach($vars as $key => $value) {
+            $_SESSION[$key] = $value;
+        }
+    }
+
+
+    public function unsetSession(string $key)
+    {
+        unset($_SESSION[$key]);
+    }
 }
