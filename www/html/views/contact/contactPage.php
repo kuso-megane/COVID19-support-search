c7078ceb66fdb8fb92d51d666032a168417413e4
kuso-megane <orikazu0824@oritawakeinoMacBook-puro.local>
2021-04-28 01:49:53 +0900

    古いお問い合わせページ削除、お問い合わせページのベータ版を作成

--- www/html/views/contact/contactPage.php
+++ www/html/views/contact/contactPage.php
@@ -1,3 +1,28 @@
 <?php
-    echo '問あわせページ';
+
+use myapp\config\AppConfig;
+use myapp\config\ViewsConfig;
+
 ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?php echo "お問い合わせ -" . AppConfig::TITLE; ?></title>
+        <link rel="stylesheet" type="text/css" href="<?php echo ViewsConfig::STYLE_SHEET_URL. 'subContents/contact.css'; ?>" >
+        <?php require ViewsConfig::COMPONENTS_PATH. 'commonHead.php'; ?>
+    </head>
+    <body>
+        <?php require ViewsConfig::COMPONENTS_PATH . 'header.php'; ?>
+
+        <main>
+            <div class="center">
+                <div id="contact" class="main--single">
+                    <h3>お問い合わせフォーム</h3>
+                    <p>今後実装予定です。至急のお問い合わせは、以下のアドレスへよろしくお願いいたします。</p>
+                    <p>アドレス: <span class="bold"><?php echo $adress; ?></span></p>
+                </div>
+            </div>
+        </main>
+    </body>
+</html>
