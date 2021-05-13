891ceef08cf0ed6f436d6758dead6e268fa70ec9
kuso-megane <orikazu0824@oritawakeinoMacBook-puro.local>
2021-05-14 05:59:46 +0900

    googleのサイト所有権確認のmetaタグをトップページに貼り付け

--- www/html/views/search/index.php
+++ www/html/views/search/index.php
@@ -11,6 +11,7 @@ use myapp\config\ViewsConfig;
         <meta name="description" content="コロナで仕事がない、住居、DV、生活費、食料、解雇・休業、ひとり親、外国籍に関する悩みなどまで幅広く、民間の活動を中心に支援・相談窓口・制度を探すことができます！">
         <link rel="stylesheet" type="text/css" href="<?php echo ViewsConfig::STYLE_SHEET_URL. 'search/index.css'; ?>">
         <?php require ViewsConfig::COMPONENTS_PATH. 'commonHead.php'; ?>
+        <meta name="google-site-verification" content="6EULL4U5dwzfYN0B6Ktf_dJj4BRnfCf5Kgdwo7ct4nU" />
     </head>
     <body>
         <?php require ViewsConfig::COMPONENTS_PATH . 'header.php'; ?>
