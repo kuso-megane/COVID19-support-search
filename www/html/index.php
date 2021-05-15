858e05f5d6cd626f2f7da9b82a3a8f620c150180
kuso-megane <orikazu0824@oritawakeinoMacBook-puro.local>
2021-05-15 12:26:36 +0900

    google search consoleサイト所有権metaタグを、実質トップページから、DirectoryIndexのトップページへ移動

--- www/html/index.php
+++ www/html/index.php
@@ -1,3 +1,9 @@
+
+<head>
+    <!--for google search console-->
+    <meta name="google-site-verification" content="6EULL4U5dwzfYN0B6Ktf_dJj4BRnfCf5Kgdwo7ct4nU" />
+</head>
+
 <?php
 
 //apacheのDirectoryIndex, ドメイン/ でアクセスされたとき、実際のtopページへredirect
