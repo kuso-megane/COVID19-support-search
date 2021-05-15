ab5158cd82f7967d14605ebd4eb6b7ded0c54b91
kuso-megane <orikazu0824@oritawakeinoMacBook-puro.local>
2021-05-15 12:32:45 +0900

    リダイレクトするため擬似トップページからheader消去

--- www/html/index.php
+++ www/html/index.php
@@ -1,9 +1,3 @@
-
-<head>
-    <!--for google search console-->
-    <meta name="google-site-verification" content="6EULL4U5dwzfYN0B6Ktf_dJj4BRnfCf5Kgdwo7ct4nU" />
-</head>
-
 <?php
 
 //apacheのDirectoryIndex, ドメイン/ でアクセスされたとき、実際のtopページへredirect
