d38de200ac57d04332af73fdf7820dfedffe048e
kuso-megane <orikazu0824@oritawakeinoMacBook-puro.local>
2021-05-16 14:31:27 +0900

    モバイルフレンドリーのため、もろもろfont-size調整

--- www/html/views/article/_list.php
+++ www/html/views/article/_list.php
@@ -16,9 +16,8 @@ use myapp\config\ViewsConfig;
         <main>
             <div class="center">
                 <div id="articles" class="main--single left">
-                    <div id="articles--title">
-                        <h3 class="center">支援・制度利用時のお役立ち情報</h3>
-                    </div>
+                    
+                    <h3 id="articles--title" class=>支援・制度利用時のお役立ち情報</h3>
 
                     <?php foreach ($articleInfos as $category => $articleInfoItems): ?>
 
