9378c52dc1aa92cc4e9d3a4b3a7330c3767c0002
jad_109 <horiyuki9811@gmail.com>
2021-05-17 20:15:37 +0900

    seo meta description　変更

--- www/html/views/search/index.php
+++ www/html/views/search/index.php
@@ -8,7 +8,8 @@ use myapp\config\ViewsConfig;
 <html>
     <head>
         <title><?php echo "すぐに使える支援・相談窓口探しなら" . AppConfig::TITLE; ?></title>
-        <meta name="description" content="コロナで仕事がない、住居、DV、生活費、食料、解雇・休業、ひとり親、外国籍に関する悩みなどまで幅広く、民間の活動を中心に支援・相談窓口・制度を探すことができます！">
+        <meta name="description" content="民間やNPOの活動を中心に支援・相談窓口・制度を探すことができる検索システムです。住まい、DV、食料、コロナで仕事・バイトが解雇・休業、シングルマザー/ファザー、外国籍に関する悩みなどまで幅広く対応しています。困ったときはためらわずにご利用ください！
+">
         <link rel="stylesheet" type="text/css" href="<?php echo ViewsConfig::STYLE_SHEET_URL. 'search/index.css'; ?>">
         <?php require ViewsConfig::COMPONENTS_PATH. 'commonHead.php'; ?>
         <meta name="google-site-verification" content="QUhvKEg1Tym9PdmUbMMUQPB82zF6iKbccQ-EN_Pcg4A" />
