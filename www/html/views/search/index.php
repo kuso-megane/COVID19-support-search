7d06b6128a5ae8ead72e9ade2b3abd4897b6b953
kuso-megane <orikazu0824@oritawakeinoMacBook-puro.local>
2021-05-20 16:16:32 +0900

    twitterカード変更

--- www/html/views/search/index.php
+++ www/html/views/search/index.php
@@ -10,17 +10,18 @@ use myapp\config\ViewsConfig;
     <head>
         <title><?php echo "すぐに使える支援・相談窓口探しなら" . AppConfig::TITLE; ?></title>
         <meta name="description" content="コロナで仕事がない、住居、DV、生活費、食料、解雇・休業、ひとり親、外国籍に関する悩みなどまで幅広く、民間の活動を中心に支援・相談窓口・制度を探すことができます！">
+
         <!--ogp-->
         <meta property="og:title" content="<?php echo "すぐに使える支援・相談窓口探しなら" . AppConfig::TITLE; ?>" />
         <meta property="og:image" content="<?php echo URIConfig::URI. ViewsConfig::IMG_URL. 'titleLogo/production.jpg'; ?>" />
         <meta property="og:description"
         content="コロナで仕事がない、住居、DV、生活費、食料、解雇・休業、ひとり親、外国籍に関する悩みなどまで幅広く、民間の活動を中心に支援・相談窓口・制度を探すことができます！"
         />
-        <meta name="twitter:card" content="summary" />
-        <!--ogp終わり-->
+        <meta name="twitter:card" content="summary_large_image" />
 
         <link rel="stylesheet" type="text/css" href="<?php echo ViewsConfig::STYLE_SHEET_URL. 'search/index.css'; ?>">
         <?php require ViewsConfig::COMPONENTS_PATH. 'commonHead.php'; ?>
+
         <meta name="google-site-verification" content="QUhvKEg1Tym9PdmUbMMUQPB82zF6iKbccQ-EN_Pcg4A" />
     </head>
     <body>
