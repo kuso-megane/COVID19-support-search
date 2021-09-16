3bed91042a6517daffd97e6fda660702408f40ca
kuso-megane <orikazu0824@oritawakeinoMacBook-puro.local>
2021-09-16 18:22:36 +0900

    運営者情報　更新

--- www/html/views/subContents/adminIntro.php
+++ www/html/views/subContents/adminIntro.php
@@ -17,7 +17,7 @@
             <div class="center">
                 <div id="adminIntro" class="main--single">
                     <h3 class="subTitle block-start0">&lt;サービス運営者紹介&gt;</h3>
-                    <p id="note">&#10004; 記載内容は執筆時点(2021年4月)のものです。</p>
+                    <p id="note">&#10004; 記載内容は執筆時点(2021年9月)のものです。</p>
 
                     <div class="administrators">
                         <h4><i class="fas fa-user-tie"></i> H.Y</h4>
@@ -30,14 +30,14 @@
                         <h4><i class="fas fa-glasses"></i> O.K</h4>
                         <p class="administrators--part">サイト作成、デザイン担当</p>
                         <p class="administrators--main">
-                            &emsp;上のH.Yと同じ大学の3年生。エンジニア志望。<br>&emsp;約1年間、企業のwebページ開発部門でのインターン経験があり、そこで学んだスキルを生かす機会がないか考えていたところ、友人たちとzoomで雑談しているなかで、同席していたH.Yにこのサービスの開発に誘われた。いまはゲーム開発に興味がある。
+                            &emsp;上のH.Yと同じ大学の3年生。エンジニア志望。<br>&emsp;約1年間、企業のwebページ開発部門でのインターン経験があり、そこで学んだスキルを生かす機会がないか考えていたところ、友人たちとzoomで雑談しているなかで、同席していたH.Yにこのサービスの開発に誘われた。いまはゲーム開発にも興味がある。
                         </p>
                     </div>
                     <div class="administrators">
                         <h4><i class="fas fa-pencil-ruler"></i> K.K</h4>
                         <p class="administrators--part">タイトルロゴ、サイトデザイン(一部)担当</p>
                         <p class="administrators--main">
-                            &emsp;上のO.Kの友人。現在は、関東の大学を卒業し、デザイナーを目指して就職活動中。<br>&emsp;就活で忙しいなか、O.Kの誘いで協力してもらった。ちなみにO.Kのtwitterアイコンも彼が作ったもの。
+                            &emsp;上のO.Kの友人。現在は、フロントエンドエンジニアとして内定。<br>&emsp;当時、就活で忙しいなか、O.Kの誘いで協力してもらった。ちなみにO.Kのtwitterアイコンも彼が作ったもの。
                         </p>
                     </div>
 
