9b57b7b51f975246cb553bd39b4dda8f1312b3b3
kuso-megane <orikazu0824@oritawakeinoMacBook-puro.local>
2021-09-18 08:50:46 +0900

    views/components/commonHeadにreact関連追加(dev)

--- www/html/views/components/commonHead.php
+++ www/html/views/components/commonHead.php
@@ -1,3 +1,15 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!--Font Awesome-->
 <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+
+<!--React-->
+<!--
+<script src="https://unpkg.com/react@17/umd/react.production.min.js" crossorigin></script>
+<script src="https://unpkg.com/react-dom@17/umd/react-dom.production.min.js" crossorigin></script>
+-->
+
+<!--dev-->
+<script src="https://unpkg.com/react@17/umd/react.development.js" crossorigin></script>
+<script src="https://unpkg.com/react-dom@17/umd/react-dom.development.js" crossorigin></script>
