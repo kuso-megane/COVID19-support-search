d5e933e08888e518044b12578c23665ca4c042e7
kuso-megane <orikazu0824@oritawakeinoMacBook-puro.local>
2021-09-18 17:41:49 +0900

    views/components/commonHead react導入中止

--- www/html/views/components/commonHead.php
+++ www/html/views/components/commonHead.php
@@ -3,13 +3,3 @@
 
 <!--Font Awesome-->
 <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
-
-<!--React-->
-<!--
-<script src="https://unpkg.com/react@17/umd/react.production.min.js" crossorigin></script>
-<script src="https://unpkg.com/react-dom@17/umd/react-dom.production.min.js" crossorigin></script>
--->
-
-<!--dev-->
-<script src="https://unpkg.com/react@17/umd/react.development.js" crossorigin></script>
-<script src="https://unpkg.com/react-dom@17/umd/react-dom.development.js" crossorigin></script>
