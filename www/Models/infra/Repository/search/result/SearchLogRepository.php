ac7c5e388230f7ca7b9b949ea29095abac29ec76
kuso-megane <orikazu.0824@i.softbank.jp>
2022-11-29 21:00:48 +0900

    infra/Repository/search/result/SearchLogRepository 設計

---
+++ www/Models/infra/Repository/search/result/SearchLogRepository.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace infra\Repository\search\result;
+
+use domain\search\result\RepositoryPort\SearchLogRepositoryPort;
+use infra\database\src\SearchLogTable;
+use PDOException;
+
+class SearchLogRepository implements SearchLogRepositoryPort
+{
+
+    private $table;
+
+    public function __construct()
+    {
+        $this->table = new SearchLogTable;
+    }
+
+    public function updateSearchLog(int $trouble_id, int $area_id, bool $is_only_foreign_ok): bool
+    {
+        try {
+            $this->table->update($trouble_id, $area_id, $is_only_foreign_ok);
+        }
+        catch(PDOException $e) {
+            throw $e;
+            return FALSE;
+        }
+
+        return TRUE;
+    }
+
+}
