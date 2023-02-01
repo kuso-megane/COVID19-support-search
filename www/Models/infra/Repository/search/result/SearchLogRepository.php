d54f389fb79c3a8feef658dad574865142239859
kuso-megane <orikazu.0824@i.softbank.jp>
2023-02-01 20:57:09 +0900

    SearchLogRepository 引数ミス訂正

--- www/Models/infra/Repository/search/result/SearchLogRepository.php
+++ www/Models/infra/Repository/search/result/SearchLogRepository.php
@@ -19,7 +19,7 @@ class SearchLogRepository implements SearchLogRepositoryPort
     public function updateSearchLog(int $trouble_id, int $area_id, bool $is_only_foreign_ok): bool
     {
         try {
-            $this->table->create($trouble_id, $area_id, $is_only_foreign_ok);
+            $this->table->create($area_id, $trouble_id, $is_only_foreign_ok);
         }
         catch(PDOException $e) {
             throw $e;
